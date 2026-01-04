abb6957981796cbf6d8ffa4e794b6c91a3c7d5b7
Technicbuilder <151921436+Technicbuilder@users.noreply.github.com>
2026-01-04 18:52:20 +0000

    Add TCP and UDP scanning methods to PortScanner
    Refactored port scanning methods to include TCP and UDP scanning functionalities. Updated the scan range logic to handle both protocols and adjusted timeout settings.
    
    Increased timeout from 0.1 -> 2 (So delayed responses can reach),
    increased threads from 100 -> 500 
    (increased threads due to longer timeout, 500 is not too aggressive )

--- scanner.py
+++ scanner.py
@@ -12,34 +12,34 @@ class PortScanner:
         self.instructions = ''
         self.services = {}
 
-    def scan(self, host, port, timeout=0.1):
+    def scan_TCP(self, host, port, timeout=2):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(timeout)
 
         try:
             result = sock.connect_ex((host, port))
             if result == 0:
-                self.available_ports.append(port)
+                self.available_ports.append((port, 'tcp'))
 
                 try:
                     sock.send(b'\r\n')
                     banner = sock.recv(1024).decode(errors='ignore').strip()
 
                     if banner:
-                        self.services[port] = banner
+                        self.services[(port, 'tcp')] = banner
                     else:
                         try:
-                            self.services[port] = socket.getservbyport(port)
+                            self.services[(port, 'tcp')] = socket.getservbyport(port)
 
                         except:
-                            self.services[port] = "Uknown Service"
+                            self.services[(port, 'tcp')] = "Uknown Service"
 
                 except:
                     try:
-                        self.services[port] = socket.getservbyport(port)
+                        self.services[(port, 'tcp')] = socket.getservbyport(port)
 
                     except:
-                        self.services[port] = 'Uknown Service'
+                        self.services[(port, 'tcp')] = 'Uknown Service'
 
         except socket.gaierror:
             return f'URL/IP {host} could not be resolved'
@@ -50,22 +50,51 @@ class PortScanner:
         finally:
             sock.close()
 
+    def scan_UDP(self, host, port, timeout=2):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(timeout)
+
+        try:
+            sock.sendto(b'', (host, port))
+            sock.recvfrom(1024) #   if open
+            self.available_ports.append((port, 'udp'))
+
+            try:
+                self.services[(port, 'udp')] = socket.getservbyport(port, 'udp')
+
+            except:
+                self.services[(port, 'udp')] = 'Uknown Service'
+
+        except socket.timeout:
+            pass    #   port has been opened or filtered
+
+        except ConnectionRefusedError:
+            pass    #   port closed
+
+        finally:
+            sock.close()
+
+
     def scan_range(self):
 
         number_of_ports = self.end_port - self.start_port + 1                   #   scans port ranges using the single
-        if number_of_ports < 500:                                               #   port scanner function above
+        if number_of_ports < 20:                                               #   port scanner function above
             for host in self.computers:
                 for port in range(int(self.start_port), int(self.end_port) + 1):
-                    self.scan(host, port)
+                    self.scan_TCP(host, port)
+                    self.scan_UDP(host, port)
+
 
         else:
 
-            threads = min(100, number_of_ports)
+            threads = min(500, number_of_ports)
             for host in self.computers:
-                with ThreadPoolExecutor(max_workers=threads) as executor:  # multithreaded approach if number of ports to be scanned is > 100
-                    scans = {
-                        executor.submit(self.scan, host, port): port for port in range(self.start_port, self.end_port + 1)
-                    }
+                with ThreadPoolExecutor(max_workers=threads) as executor:
+                    scans = {}
+
+                    for port in range(self.start_port, self.end_port + 1):
+                        scans[executor.submit(self.scan_TCP, host, port)] = port
+                        scans[executor.submit(self.scan_UDP, host, port)] = port
 
                     for future in as_completed(scans):
                         port = scans[future]
@@ -75,7 +104,3 @@ class PortScanner:
 
                         except Exception as e:
                             return f'Error scanning port {port}: {e}'
-
-
-
-
